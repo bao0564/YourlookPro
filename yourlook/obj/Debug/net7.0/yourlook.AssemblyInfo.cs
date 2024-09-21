14d9ba79619322ac6c5cf545965b7a995713f7db
bao0564 <baokun959@gmail.com>
2024-09-22 00:02:49 +0700

    22/9

--- yourlook/obj/Debug/net7.0/yourlook.AssemblyInfo.cs
+++ yourlook/obj/Debug/net7.0/yourlook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("yourlook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cff4bd843d5e4e450521466b8da9447c50fcb877")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d3400d7e6425784d45795ac0aab51477a75094c")]
 [assembly: System.Reflection.AssemblyProductAttribute("yourlook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("yourlook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
